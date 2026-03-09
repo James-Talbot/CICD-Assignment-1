36253fd645be837feedc4ab259151e9723a1dbb3
James-Talbot <talbotbud@gmail.com>
2026-03-09 12:35:05 +0000

    initial commit

---
+++ src/test/java/com/example/cicdassignment/CicdAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
